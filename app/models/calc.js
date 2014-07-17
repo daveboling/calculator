83d9a4c5bcd27d433b518cd7ce4b9cd3a21d3a46
David Boling <daviddboling@gmail.com>
2014-07-17 11:43:22 -0500

    new functions

--- app/models/calc.js
+++ app/models/calc.js
@@ -6,4 +6,18 @@ Calc.add = function(x,y){
   return x + y;
 };
 
+Calc.subtract = function(x,y){
+  return x - y;
+};
+
+Calc.distance = function(point1, point2){
+  var x1 = point1.x;
+  var y1 = point1.y;
+
+  var x2 = point2.x;
+  var y2 = point2.y;
+
+  return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+};
+
 module.exports = Calc;
--- test/unit/models/test_calc.js
+++ test/unit/models/test_calc.js
@@ -13,4 +13,16 @@ describe('Calc', function() {
        expect(sum).to.equal(5);
     });
   });
+  describe('.subtract', function() {
+    it('should subtract two numbers', function(){
+        var subtract = Calc.subtract(2,3);
+        expect(subtract).to.equal(-1);
+    });
+  });
+  describe('.distance', function() {
+    it('should calculate the distance between two points', function(){
+      var distance = Calc.distance({x: -1, y: 3}, {x: 4, y: 15});
+      expect(distance).to.equal(13);
+    });
+  });
 });
